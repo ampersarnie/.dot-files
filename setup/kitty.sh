b9766ecaf5010dea4b6f5af8d1d31c25ed3ba9cb
Paul Taylor <paul.taylor@hey.com>
2023-02-26 14:57:38 +0000

    fix: enable xmodmap

--- setup/kitty.sh
+++ setup/kitty.sh
@@ -4,7 +4,7 @@
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+    ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/ && 2> /dev/null
     cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
     cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
     sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
